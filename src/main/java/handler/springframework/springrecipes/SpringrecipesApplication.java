bfdcda0d7947e249c2f97c27c06757490e17e4a8
Mortimus <llcoole93@gmail.com>
2021-04-09 17:12:03 -0400

    init

---
+++ src/main/java/handler/springframework/springrecipes/SpringrecipesApplication.java
@@ -0,0 +1,13 @@
+package handler.springframework.springrecipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrecipesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringrecipesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/handler/springframework/springrecipes/SpringrecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package handler.springframework.springrecipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringrecipesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
